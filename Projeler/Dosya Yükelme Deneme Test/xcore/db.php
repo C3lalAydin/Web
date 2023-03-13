e84c571af258505d6d4e063c3495c2fc8fd7ce67
C3lalAydin <97107273+C3lalAydin@users.noreply.github.com>
2023-03-13 13:37:08 +0300

    Update db.php

--- Projeler/Dosya Yükelme Deneme Test/xcore/db.php
+++ Projeler/Dosya Yükelme Deneme Test/xcore/db.php
@@ -5,12 +5,7 @@ $db   = 'chet';
 $user = 'root';
 $pass = '';
 
-/*
-$host = 'sql309.epizy.com';
-$db   = 'epiz_32077589_test';
-$user = 'epiz_32077589';
-$pass = '2nNJe9ZpgZGi';
-*/
+
 $sql = "mysql:host=$host;dbname=$db";
 try {
      $pdo = new PDO($sql, $user, $pass);
